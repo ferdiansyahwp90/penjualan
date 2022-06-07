1e3bb0d0310ab8a7d90fd20f8f17b76b89919e2d
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2022-06-07 18:22:09 +0700

    Fixing Controller

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -31,10 +31,10 @@
                       <div class="user-bg"> <img width="100%" alt="user" src="{{ asset('assets/plugins/images/large/background.jpg') }}">
                           <div class="overlay-box">
                               <div class="user-content">
-                                  <a href="javascript:void(0)"><img src="{{ asset('assets/plugins/images/users/1.jpg') }}"
+                                  <a href="javascript:void(0)"><img src="{{ asset('assets/plugins/images/users/user.jpg') }}"
                                           class="thumb-lg img-circle" alt="img"></a>
-                                  <h4 class="text-white mt-2">User Name</h4>
-                                  <h5 class="text-white mt-2">info@myadmin.com</h5>
+                                  <h4 class="text-white mt-2">{{ Auth::user()->name }}</h4>
+                                  <h5 class="text-white mt-2">{{ Auth::user()->email }}</h5>
                               </div>
                           </div>
                       </div>
@@ -48,15 +48,15 @@
                       <div class="card-body">
                           <form class="form-horizontal form-material">
                               <div class="form-group mb-4">
-                                  <label class="col-md-12 p-0">Full Name</label>
+                                  <label class="col-md-12 p-0">Name</label>
                                   <div class="col-md-12 border-bottom p-0">
-                                      <input type="text" placeholder="Johnathan Doe"
+                                      <input type="text" placeholder="{{ Auth::user()->name }}"
                                           class="form-control p-0 border-0"> </div>
                               </div>
                               <div class="form-group mb-4">
                                   <label for="example-email" class="col-md-12 p-0">Email</label>
                                   <div class="col-md-12 border-bottom p-0">
-                                      <input type="email" placeholder="johnathan@admin.com"
+                                      <input type="email" placeholder="{{ Auth::user()->email }}"
                                           class="form-control p-0 border-0" name="example-email"
                                           id="example-email">
                                   </div>
